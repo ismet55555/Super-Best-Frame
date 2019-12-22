d873b282048e9cfb3b54cbce034a94cc21ba81ce
Ismet Handzic <Ismet.Handzic@gmail.com>
2019-12-21 21:21:52 -0500

    only load paths not image objects

--- picture-frame.py
+++ picture-frame.py
@@ -24,13 +24,13 @@ imageDirectory  = os.path.join(currentDirectory, imageDirectory) + "//"
 # Array of opencv image objects
 images = []
 imageNames = []
+imagePaths = []
+
 
 # Loop through all images in the directory
 for filename in os.listdir(imageDirectory):
-    # Creating the image object for each image
-    imageFileObject = cv2.imread(imageDirectory + filename, cv2.IMREAD_UNCHANGED)
-    # Add image object to array
-    images.append(imageFileObject)
+    # Add image path
+    imagePaths.append(imageDirectory + filename)
     # Add the name of the image to an array
     imageNames.append(filename)
 
@@ -45,12 +45,15 @@ try:
     while True:
         # Picking a random image form directory
         random.seed()
-        imageIndex = random.randint(0, len(images)-1)
+        imageIndex = random.randint(0, len(imagePaths)-1)
 
-        print('Showing Image {} of {} ({})'.format(imageIndex, len(images), imageNames[imageIndex]))
+        print('Showing Image {} of {} ({})'.format(imageIndex, len(imagePaths), imageNames[imageIndex]))
+
+        # Load the image
+        imageFileObject = cv2.imread(imagePaths[imageIndex], cv2.IMREAD_UNCHANGED)
 
         # Showing/Updating Image
-        cv2.imshow('Image', images[imageIndex])
+        cv2.imshow('Image', imageFileObject)
         
         # Waiting 2 seconds and watching out for key press to quit
         if cv2.waitKey(image_delay) == ord(' ') :
