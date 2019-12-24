d0480eaca18f6461552c88f87ec507635992cd6d
Ismet Handzic <Ismet.Handzic@gmail.com>
2019-12-24 17:03:45 -0500

    fade effect - different image sizes

--- picture-frame.py
+++ picture-frame.py
@@ -4,13 +4,46 @@ import sys
 import os
 import random
 import cv2
+import imutils
+from math import floor, ceil
 import numpy as np
+import logging
+from logging.handlers import RotatingFileHandler
+import subprocess
 
+
+###############################################################################
+
+def create_logger():
+    # Setting up logger and a local log file
+    file_handler = logging.handlers.RotatingFileHandler(filename='picture-frame.log', mode='w', maxBytes=10000000, backupCount=0)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(level=logging.INFO,
+                        format='[Picture-Frame] - [%(asctime)s] - %(levelname)-10s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        handlers=[file_handler, stdout_handler])
+
+def get_screen_size():
+    # Finding the size of the current screen
+    processes = subprocess.Popen(["xrandr | grep '*'"],stdout=subprocess.PIPE, shell=True).stdout.read().decode('utf-8').strip().split(' ')
+    # TODO: Detect a return of no display
+    # Remove blanks and new line characters
+    processes = [i for i in processes if (i and i != '\n')] 
+    # Only find items containing "x" and get the first listed
+    # TODO: Find a way to know where images are displayed
+    processes = [i for i in processes if 'x' in i][0]
+    # Split into width and height
+    width_px = int(processes.split('x')[0])
+    height_px = int(processes.split('x')[1])
+    # TODO: Error handling
+    return width_px, height_px
+
+###############################################################################
 
 def effect_none(img_end):
     cv2.imshow('Image', img_end)
 
-def effect_fade_in(img_begin, img_end, len=10, fade_delay=1):
+def effect_fade(img_begin, img_end, len=10, fade_delay=1):
     fading = True
     while fading:
         for index in range(0, len):
@@ -24,33 +57,21 @@ def effect_fade_in(img_begin, img_end, len=10, fade_delay=1):
         fading = False
 
 
-# SCREEN SIZE: xrandr | grep '*'
-
-def effect_fade_in_2(image1, image2):
-
-    foreground, background = image1.copy(), image2.copy()
-
-    foreground_height = foreground.shape[0]
-    foreground_width = foreground.shape[1]
-    alpha = 0.5
-
-    # do composite on the upper-left corner of background image.
-    blended_portion = cv2.addWeighted(foreground,
-                alpha,
-                background[:foreground_height,:foreground_width,:],
-                1 - alpha,
-                0,
-                background)
-    background[:foreground_height,:foreground_width,:] = blended_portion
-    cv2.imshow('Image', background)
-
-    input('asdf')
+###############################################################################
+###############################################################################
 
 
-print('===================')
-print('Start')
-print('')
+# Creating the logger for this python script
+create_logger()
+logging.info('Picture frame python script started')
 
+# Finding the screen width and height
+screen_width_px, screen_height_px = get_screen_size()
+screen_aspect_ratio = screen_width_px / screen_height_px
+logging.info('Display/Screen size determined:')
+logging.info('    Width:        {}'.format(screen_width_px))
+logging.info('    Height:       {}'.format(screen_height_px))
+logging.info('    Aspect Ratio: {:.2f}'.format(screen_aspect_ratio))
 
 # Image delay
 image_delay = 3000
@@ -67,6 +88,7 @@ imageNames = []
 imagePaths = []
 
 # Map out through all images in the directory
+logging.info('Mapping out all available image files in directory: {}'.format(imageDirectory))
 for filename in os.listdir(imageDirectory):
     # Only consider files, not directories
     if os.path.isfile(imageDirectory + filename):
@@ -74,57 +96,104 @@ for filename in os.listdir(imageDirectory):
         imagePaths.append(imageDirectory + filename)
         # Add the name of the image to an array
         imageNames.append(filename)
+logging.info('Successfully mapped out {} images'.format(len(imagePaths)))
 
-
+logging.info('Configuring full screen image window ...')
 # Opening openCV window
 cv2.namedWindow('Image', cv2.WND_PROP_FULLSCREEN)
 # Setting up openCV window for full screen mode
 cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-#TODO: imshow a screen size blank image
-
-# Beginning image index
+# Initializing beginning and ending image index
 img_begin_index = -1
 img_end_index = -1
 
-# try:
-while True:
-    # Picking a random image from directory - Target
-    random.seed()
-    while img_end_index == img_begin_index:
-        img_end_index = random.randint(0, len(imagePaths)-1)
+logging.info('Beginning picture frame image rotation. Use SPACEBAR to quit ...')
+try:
+    while True:
+        # Picking a random image from directory - Target
+        random.seed()
+        while img_end_index == img_begin_index:
+            img_end_index = random.randint(0, len(imagePaths)-1)
 
-    print('------')
-    print(img_begin_index, img_end_index)
+        logging.info('Showing image index {} ("{}")'.format(img_end_index, imageNames[img_end_index]))
 
-    # print('Showing Image {} of {} ({})'.format(img_end_index, len(imagePaths), imageNames[img_end_index]))
+        # Load the starting image images
+        if img_begin_index == -1:
+            imageFileObject_begin = np.zeros((4000, 6000, 3), np.uint8)
+        else:
+            imageFileObject_begin = cv2.imread(imagePaths[img_begin_index], cv2.IMREAD_UNCHANGED)
+        img_begin_aspect_ratio = imageFileObject_begin.shape[1] / imageFileObject_begin.shape[0]
 
-    # Load the starting image images
-    if img_begin_index == -1:
-        imageFileObject_begin = np.zeros((4000, 6000, 3), np.uint8)
-    else:
-        imageFileObject_begin = cv2.imread(imagePaths[img_begin_index], cv2.IMREAD_UNCHANGED)
+        # Loading the ending image
+        imageFileObject_end = cv2.imread(imagePaths[img_end_index], cv2.IMREAD_UNCHANGED)
+        img_end_aspect_ratio = imageFileObject_end.shape[1] / imageFileObject_end.shape[0]
 
-    # Loading the ending image
-    imageFileObject_end = cv2.imread(imagePaths[img_end_index], cv2.IMREAD_UNCHANGED)
+        # Resize and grow into screen
+        if screen_aspect_ratio > 1:
+            # Landscape screen orientation
+            # Resize to screen height
+            imageFileObject_begin = imutils.resize(imageFileObject_begin, height=screen_height_px)
+            imageFileObject_end = imutils.resize(imageFileObject_end, height=screen_height_px)
 
-    # Fade transition effect
-    effect_fade_in(imageFileObject_begin, imageFileObject_end)
+            # Finding the filler border size
+            img_fill_border_begin = floor((screen_width_px - imageFileObject_begin.shape[1]) / 2)
+            img_fill_border_begin = 0 if img_fill_border_begin < 0 else img_fill_border_begin
+            img_fill_border_end = floor((screen_width_px - imageFileObject_end.shape[1]) / 2)
+            img_fill_border_end = 0 if img_fill_border_end < 0 else img_fill_border_end
 
-    # Waiting 2 seconds and watching out for key press to quit
-    if cv2.waitKey(image_delay) == ord(' ') :
-        break
+            # Add the border
+            imageFileObject_begin = cv2.copyMakeBorder(imageFileObject_begin, top=0, bottom=0, left=img_fill_border_begin, right=img_fill_border_begin, borderType=cv2.BORDER_CONSTANT)
+            imageFileObject_end = cv2.copyMakeBorder(imageFileObject_end, top=0, bottom=0, left=img_fill_border_end, right=img_fill_border_end, borderType=cv2.BORDER_CONSTANT) 
 
-    # The next beginning image is the last end image
-    img_begin_index = img_end_index
+            # Account for slight difference when border is added
+            avg_width = ceil((imageFileObject_begin.shape[1] + imageFileObject_end.shape[1]) / 2)
+            avg_height = ceil((imageFileObject_begin.shape[0] + imageFileObject_end.shape[0]) / 2)
 
-# except Exception as e:
-#     print(e)
+            # Resize to average dimensions
+            imageFileObject_begin = imutils.resize(imageFileObject_begin, width=avg_width, height=avg_height)
+            imageFileObject_end = imutils.resize(imageFileObject_end, width=avg_width, height=avg_height)
+        else:
+            # Portrait screen orientation
+            # Resize to screen width
+            imageFileObject_begin = imutils.resize(imageFileObject_begin, height=screen_width_px)
+            imageFileObject_end = imutils.resize(imageFileObject_end, height=screen_width_px)
+
+            # Finding the filler border size
+            img_fill_border_begin = floor((screen_height_px - imageFileObject_begin.shape[0]) / 2)
+            img_fill_border_begin = 0 if img_fill_border_begin < 0 else img_fill_border_begin
+            img_fill_border_end = floor((screen_height_px - imageFileObject_end.shape[0]) / 2)
+            img_fill_border_end = 0 if img_fill_border_end < 0 else img_fill_border_end
+
+            # Add the border
+            imageFileObject_begin = cv2.copyMakeBorder(imageFileObject_begin, top=img_fill_border_begin, bottom=img_fill_border_begin, left=0, right=0, borderType=cv2.BORDER_CONSTANT)
+            imageFileObject_end = cv2.copyMakeBorder(imageFileObject_end, top=img_fill_border_end, bottom=img_fill_border_end, left=0, right=0, borderType=cv2.BORDER_CONSTANT) 
+
+            # Account for slight difference when border is added
+            avg_width = ceil((imageFileObject_begin.shape[1] + imageFileObject_end.shape[1]) / 2)
+            avg_height = ceil((imageFileObject_begin.shape[0] + imageFileObject_end.shape[0]) / 2)
+
+            # Resize to average dimensions
+            imageFileObject_begin = imutils.resize(imageFileObject_begin, width=avg_width, height=avg_height)
+            imageFileObject_end = imutils.resize(imageFileObject_end, width=avg_width, height=avg_height)
+
+        # Fade transition effect
+        effect_fade(imageFileObject_begin, imageFileObject_end)
+
+        # Waiting 2 seconds and watching out for key press to quit
+        if cv2.waitKey(image_delay) == ord(' '):
+            logging.info('SPACEBAR key detected. Exiting ...')
+            break
+
+        # The next beginning image is the last end image
+        img_begin_index = img_end_index
+
+except Exception as e:
+    print(e)
     
-# finally:
-#Close all open openCV windows
-cv2.destroyAllWindows()
+finally:
+    #Close all open openCV windows
+    logging.info('Closing all open image windows')
+    cv2.destroyAllWindows()
 
-print('')
-print('DONE!')
-print('===================')
+logging.info('Picture frame python application has ended')
