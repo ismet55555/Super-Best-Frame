7d5cbbbb76b7cdc9c41230cf75be760b4212597c
Ismet Handzic <Ismet.Handzic@gmail.com>
2019-12-23 23:19:45 -0500

    fade effect work

--- picture-frame.py
+++ picture-frame.py
@@ -4,6 +4,47 @@ import sys
 import os
 import random
 import cv2
+import numpy as np
+
+
+def effect_none(img_end):
+    cv2.imshow('Image', img_end)
+
+def effect_fade_in(img_begin, img_end, len=10, fade_delay=1):
+    fading = True
+    while fading:
+        for index in range(0, len):
+            fade_in_radio = index/float(len)
+            mergedImageFrame = cv2.addWeighted(img_begin, 1 - fade_in_radio, img_end, fade_in_radio, 0)
+            cv2.imshow('Image', mergedImageFrame)
+            if cv2.waitKey(fade_delay) == ord(' '):
+                fading = False
+                break
+        cv2.imshow('Image', img_end)
+        fading = False
+
+
+# SCREEN SIZE: xrandr | grep '*'
+
+def effect_fade_in_2(image1, image2):
+
+    foreground, background = image1.copy(), image2.copy()
+
+    foreground_height = foreground.shape[0]
+    foreground_width = foreground.shape[1]
+    alpha = 0.5
+
+    # do composite on the upper-left corner of background image.
+    blended_portion = cv2.addWeighted(foreground,
+                alpha,
+                background[:foreground_height,:foreground_width,:],
+                1 - alpha,
+                0,
+                background)
+    background[:foreground_height,:foreground_width,:] = blended_portion
+    cv2.imshow('Image', background)
+
+    input('asdf')
 
 
 print('===================')
@@ -22,17 +63,18 @@ imageDirectory  = "Images"
 imageDirectory  = os.path.join(currentDirectory, imageDirectory) + "//"
 
 # Array of opencv image objects
-images = []
 imageNames = []
 imagePaths = []
 
-
-# Loop through all images in the directory
+# Map out through all images in the directory
 for filename in os.listdir(imageDirectory):
-    # Add image path
-    imagePaths.append(imageDirectory + filename)
-    # Add the name of the image to an array
-    imageNames.append(filename)
+    # Only consider files, not directories
+    if os.path.isfile(imageDirectory + filename):
+        # Add image path
+        imagePaths.append(imageDirectory + filename)
+        # Add the name of the image to an array
+        imageNames.append(filename)
+
 
 # Opening openCV window
 cv2.namedWindow('Image', cv2.WND_PROP_FULLSCREEN)
@@ -41,30 +83,47 @@ cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 #TODO: imshow a screen size blank image
 
-try:
-    while True:
-        # Picking a random image form directory
-        random.seed()
-        imageIndex = random.randint(0, len(imagePaths)-1)
+# Beginning image index
+img_begin_index = -1
+img_end_index = -1
 
-        print('Showing Image {} of {} ({})'.format(imageIndex, len(imagePaths), imageNames[imageIndex]))
+# try:
+while True:
+    # Picking a random image from directory - Target
+    random.seed()
+    while img_end_index == img_begin_index:
+        img_end_index = random.randint(0, len(imagePaths)-1)
 
-        # Load the image
-        imageFileObject = cv2.imread(imagePaths[imageIndex], cv2.IMREAD_UNCHANGED)
+    print('------')
+    print(img_begin_index, img_end_index)
 
-        # Showing/Updating Image
-        cv2.imshow('Image', imageFileObject)
-        
-        # Waiting 2 seconds and watching out for key press to quit
-        if cv2.waitKey(image_delay) == ord(' ') :
-            break
+    # print('Showing Image {} of {} ({})'.format(img_end_index, len(imagePaths), imageNames[img_end_index]))
 
-except Exception as e:
-    print(e)
+    # Load the starting image images
+    if img_begin_index == -1:
+        imageFileObject_begin = np.zeros((4000, 6000, 3), np.uint8)
+    else:
+        imageFileObject_begin = cv2.imread(imagePaths[img_begin_index], cv2.IMREAD_UNCHANGED)
+
+    # Loading the ending image
+    imageFileObject_end = cv2.imread(imagePaths[img_end_index], cv2.IMREAD_UNCHANGED)
+
+    # Fade transition effect
+    effect_fade_in(imageFileObject_begin, imageFileObject_end)
+
+    # Waiting 2 seconds and watching out for key press to quit
+    if cv2.waitKey(image_delay) == ord(' ') :
+        break
+
+    # The next beginning image is the last end image
+    img_begin_index = img_end_index
+
+# except Exception as e:
+#     print(e)
     
-finally:
-    #Close all open openCV windows
-    cv2.destroyAllWindows()
+# finally:
+#Close all open openCV windows
+cv2.destroyAllWindows()
 
 print('')
 print('DONE!')
